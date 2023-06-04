80ca4dea5ee23c74be55d86df997ac580a40058a
David Silgado <dsilgadosalcedo@gmail.com>
2023-06-04 17:47:26 -0500

    LocalStorage HotFix

--- scripts/player.js
+++ scripts/player.js
@@ -1,11 +1,16 @@
-let player = localStorage.getItem('player')
-if (player == null) {
+let player;
+
+try {
+    player = JSON.parse(localStorage.getItem('player'));
+} catch (error) {
+    player = null
+}
+
+if (player === null) {
     let defaultPlayer = {
-        userName: 'Player',
-        recordScore: [],
-        currentScore: []
-    }
-    localStorage.setItem('player', JSON.stringify(defaultPlayer))
-} else {
-    player = JSON.parse(localStorage.getItem('player'))
+    userName: 'Player',
+    recordScore: [],
+    currentScore: []
+    };
+    localStorage.setItem('player', JSON.stringify(defaultPlayer));
 }
